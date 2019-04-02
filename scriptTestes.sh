bc2187e1386cf8457b3b757c161cfc97cffc3109
Yan <iaobs@outlook.com>
2019-04-02 15:04:55 -0300

    Script de Testes da Seção 2 do Relatório

---
+++ scriptTestes.sh
@@ -0,0 +1,240 @@
+echo "Busca em largura - 3x3 - 10x: (1)"
+python buscas.py 0 3 10
+echo "Busca em largura - 3x3 - 10x: (2)"
+python buscas.py 0 3 10
+echo "Busca em largura - 3x3 - 10x: (3)"
+python buscas.py 0 3 10
+echo "Busca em largura - 3x3 - 100x: (1)"
+python buscas.py 0 3 100
+echo "Busca em largura - 3x3 - 100x: (2)"
+python buscas.py 0 3 100
+echo "Busca em largura - 3x3 - 100x: (3)"
+python buscas.py 0 3 100
+echo "Busca em largura - 3x3 - 1000x: (1)"
+python buscas.py 0 3 1000
+echo "Busca em largura - 3x3 - 1000x: (2)"
+python buscas.py 0 3 1000
+echo "Busca em largura - 3x3 - 1000x: (3)"
+python buscas.py 0 3 1000
+echo "Busca em largura - 3x3 - 100000x: (1)"
+python buscas.py 0 3 10000
+echo "Busca em largura - 3x3 - 100000x: (2)"
+python buscas.py 0 3 10000
+echo "Busca em largura - 3x3 - 100000x: (3)"
+python buscas.py 0 3 10000
+echo "Busca em largura - 4x4 - 10x: (1)"
+python buscas.py 0 4 10
+echo "Busca em largura - 4x4 - 10x: (2)"
+python buscas.py 0 4 10
+echo "Busca em largura - 4x4 - 10x: (3)"
+python buscas.py 0 4 10
+echo "Busca em largura - 4x4 - 100x: (1)"
+python buscas.py 0 4 100
+echo "Busca em largura - 4x4 - 100x: (2)"
+python buscas.py 0 4 100
+echo "Busca em largura - 4x4 - 100x: (3)"
+python buscas.py 0 4 100
+echo "Busca em largura - 4x4 - 1000x: (1)"
+python buscas.py 0 4 1000
+echo "Busca em largura - 4x4 - 1000x: (2)"
+python buscas.py 0 4 1000
+echo "Busca em largura - 4x4 - 1000x: (3)"
+python buscas.py 0 4 1000
+echo "Busca em largura - 4x4 - 10000x: (1)"
+python buscas.py 0 4 10000
+echo "Busca em largura - 4x4 - 10000x: (2)"
+python buscas.py 0 4 10000
+echo "Busca em largura - 4x4 - 10000x: (3)"
+python buscas.py 0 4 10000
+echo "Busca em profundidade - 3x3 - 10x: (1)"
+python buscas.py 1 3 10
+echo "Busca em profundidade - 3x3 - 10x: (2)"
+python buscas.py 1 3 10
+echo "Busca em profundidade - 3x3 - 10x: (3)"
+python buscas.py 1 3 10
+echo "Busca em profundidade - 3x3 - 100x: (1)"
+python buscas.py 1 3 100
+echo "Busca em profundidade - 3x3 - 100x: (2)"
+python buscas.py 1 3 100
+echo "Busca em profundidade - 3x3 - 100x: (3)"
+python buscas.py 1 3 100
+echo "Busca em profundidade - 3x3 - 1000x: (1)"
+python buscas.py 1 3 1000
+echo "Busca em profundidade - 3x3 - 1000x: (2)"
+python buscas.py 1 3 1000
+echo "Busca em profundidade - 3x3 - 1000x: (3)"
+python buscas.py 1 3 1000
+echo "Busca em profundidade - 3x3 - 10000x: (1)"
+python buscas.py 1 3 10000
+echo "Busca em profundidade - 3x3 - 10000x: (2)"
+python buscas.py 1 3 10000
+echo "Busca em profundidade - 3x3 - 10000x: (3)"
+python buscas.py 1 3 10000
+echo "Busca em profundidade - 4x4 - 10x: (1)"
+python buscas.py 1 4 10
+echo "Busca em profundidade - 4x4 - 10x: (2)"
+python buscas.py 1 4 10
+echo "Busca em profundidade - 4x4 - 10x: (3)"
+python buscas.py 1 4 10
+echo "Busca em profundidade - 4x4 - 100x: (1)"
+python buscas.py 1 4 100
+echo "Busca em profundidade - 4x4 - 100x: (2)"
+python buscas.py 1 4 100
+echo "Busca em profundidade - 4x4 - 100x: (3)"
+python buscas.py 1 4 100
+echo "Busca em profundidade - 4x4 - 1000x: (1)"
+python buscas.py 1 4 1000
+echo "Busca em profundidade - 4x4 - 1000x: (2)"
+python buscas.py 1 4 1000
+echo "Busca em profundidade - 4x4 - 1000x: (3)"
+python buscas.py 1 4 1000
+echo "Busca em profundidade - 4x4 - 10000x: (1)"
+python buscas.py 1 4 10000
+echo "Busca em profundidade - 4x4 - 10000x: (2)"
+python buscas.py 1 4 10000
+echo "Busca em profundidade - 4x4 - 10000x: (3)"
+python buscas.py 1 4 10000
+echo "Busca em profundidade Iterativa - 3x3 - 10x: (1)"
+python buscas.py 2 3 10
+echo "Busca em profundidade Iterativa - 3x3 - 10x: (2)"
+python buscas.py 2 3 10
+echo "Busca em profundidade Iterativa - 3x3 - 10x: (3)"
+python buscas.py 2 3 10
+echo "Busca em profundidade Iterativa - 3x3 - 100x: (1)"
+python buscas.py 2 3 100
+echo "Busca em profundidade Iterativa - 3x3 - 100x: (2)"
+python buscas.py 2 3 100
+echo "Busca em profundidade Iterativa - 3x3 - 100x: (3)"
+python buscas.py 2 3 100
+echo "Busca em profundidade Iterativa - 3x3 - 1000x: (1)"
+python buscas.py 2 3 1000
+echo "Busca em profundidade Iterativa - 3x3 - 1000x: (2)"
+python buscas.py 2 3 1000
+echo "Busca em profundidade Iterativa - 3x3 - 1000x: (3)"
+python buscas.py 2 3 1000
+echo "Busca em profundidade Iterativa - 3x3 - 10000x: (1)"
+python buscas.py 2 3 10000
+echo "Busca em profundidade Iterativa - 3x3 - 10000x: (2)"
+python buscas.py 2 3 10000
+echo "Busca em profundidade Iterativa - 3x3 - 10000x: (3)"
+python buscas.py 2 3 10000
+echo "Busca em profundidade Iterativa - 4x4 - 10x: (1)"
+python buscas.py 2 4 10
+echo "Busca em profundidade Iterativa - 4x4 - 10x: (2)"
+python buscas.py 2 4 10
+echo "Busca em profundidade Iterativa - 4x4 - 10x: (3)"
+python buscas.py 2 4 10
+echo "Busca em profundidade Iterativa - 4x4 - 100x: (1)"
+python buscas.py 2 4 100
+echo "Busca em profundidade Iterativa - 4x4 - 100x: (2)"
+python buscas.py 2 4 100
+echo "Busca em profundidade Iterativa - 4x4 - 100x: (3)"
+python buscas.py 2 4 100
+echo "Busca em profundidade Iterativa - 4x4 - 1000x: (1)"
+python buscas.py 2 4 1000
+echo "Busca em profundidade Iterativa - 4x4 - 1000x: (2)"
+python buscas.py 2 4 1000
+echo "Busca em profundidade Iterativa - 4x4 - 1000x: (3)"
+python buscas.py 2 4 1000
+echo "Busca em profundidade Iterativa - 4x4 - 10000x: (1)"
+python buscas.py 2 4 10000
+echo "Busca em profundidade Iterativa - 4x4 - 10000x: (2)"
+python buscas.py 2 4 10000
+echo "Busca em profundidade Iterativa - 4x4 - 10000x: (3)"
+python buscas.py 2 4 10000
+echo "Busca A* com heurística 1 - 3x3 - 10x: (1)"
+python buscas.py 3 3 10
+echo "Busca A* com heurística 1 - 3x3 - 10x: (2)"
+python buscas.py 3 3 10
+echo "Busca A* com heurística 1 - 3x3 - 10x: (3)"
+python buscas.py 3 3 10
+echo "Busca A* com heurística 1 - 3x3 - 100x: (1)"
+python buscas.py 3 3 100
+echo "Busca A* com heurística 1 - 3x3 - 100x: (2)"
+python buscas.py 3 3 100
+echo "Busca A* com heurística 1 - 3x3 - 100x: (3)"
+python buscas.py 3 3 100
+echo "Busca A* com heurística 1 - 3x3 - 1000x: (1)"
+python buscas.py 3 3 1000
+echo "Busca A* com heurística 1 - 3x3 - 1000x: (2)"
+python buscas.py 3 3 1000
+echo "Busca A* com heurística 1 - 3x3 - 1000x: (3)"
+python buscas.py 3 3 1000
+echo "Busca A* com heurística 1 - 3x3 - 10000x: (1)"
+python buscas.py 3 3 10000
+echo "Busca A* com heurística 1 - 3x3 - 10000x: (2)"
+python buscas.py 3 3 10000
+echo "Busca A* com heurística 1 - 3x3 - 10000x: (3)"
+python buscas.py 3 3 10000
+echo "Busca A* com heurística 1 - 4x4 - 10x: (1)"
+python buscas.py 3 4 10
+echo "Busca A* com heurística 1 - 4x4 - 10x: (2)"
+python buscas.py 3 4 10
+echo "Busca A* com heurística 1 - 4x4 - 10x: (3)"
+python buscas.py 3 4 10
+echo "Busca A* com heurística 1 - 4x4 - 100x: (1)"
+python buscas.py 3 4 100
+echo "Busca A* com heurística 1 - 4x4 - 100x: (2)"
+python buscas.py 3 4 100
+echo "Busca A* com heurística 1 - 4x4 - 100x: (3)"
+python buscas.py 3 4 100
+echo "Busca A* com heurística 1 - 4x4 - 1000x: (1)"
+python buscas.py 3 4 1000
+echo "Busca A* com heurística 1 - 4x4 - 1000x: (2)"
+python buscas.py 3 4 1000
+echo "Busca A* com heurística 1 - 4x4 - 1000x: (3)"
+python buscas.py 3 4 1000
+echo "Busca A* com heurística 1 - 4x4 - 10000x: (1)"
+python buscas.py 3 4 10000
+echo "Busca A* com heurística 1 - 4x4 - 10000x: (2)"
+python buscas.py 3 4 10000
+echo "Busca A* com heurística 1 - 4x4 - 10000x: (3)"
+python buscas.py 3 4 10000
+echo "Busca A* com heurística 2 - 3x3 - 10x: (1)"
+python buscas.py 4 3 10
+echo "Busca A* com heurística 2 - 3x3 - 10x: (2)"
+python buscas.py 4 3 10
+echo "Busca A* com heurística 2 - 3x3 - 10x: (3)"
+python buscas.py 4 3 10
+echo "Busca A* com heurística 2 - 3x3 - 100x: (1)"
+python buscas.py 4 3 100
+echo "Busca A* com heurística 2 - 3x3 - 100x: (2)"
+python buscas.py 4 3 100
+echo "Busca A* com heurística 2 - 3x3 - 100x: (3)"
+python buscas.py 4 3 100
+echo "Busca A* com heurística 2 - 3x3 - 1000x: (1)"
+python buscas.py 4 3 1000
+echo "Busca A* com heurística 2 - 3x3 - 1000x: (2)"
+python buscas.py 4 3 1000
+echo "Busca A* com heurística 2 - 3x3 - 1000x: (3)"
+python buscas.py 4 3 1000
+echo "Busca A* com heurística 2 - 3x3 - 10000x: (1)"
+python buscas.py 4 3 10000
+echo "Busca A* com heurística 2 - 3x3 - 10000x: (2)"
+python buscas.py 4 3 10000
+echo "Busca A* com heurística 2 - 3x3 - 10000x: (3)"
+python buscas.py 4 3 10000
+echo "Busca A* com heurística 2 - 4x4 - 10x: (1)"
+python buscas.py 4 4 10
+echo "Busca A* com heurística 2 - 4x4 - 10x: (2)"
+python buscas.py 4 4 10
+echo "Busca A* com heurística 2 - 4x4 - 10x: (3)"
+python buscas.py 4 4 10
+echo "Busca A* com heurística 2 - 4x4 - 100x: (1)"
+python buscas.py 4 4 100
+echo "Busca A* com heurística 2 - 4x4 - 100x: (2)"
+python buscas.py 4 4 100
+echo "Busca A* com heurística 2 - 4x4 - 100x: (3)"
+python buscas.py 4 4 100
+echo "Busca A* com heurística 2 - 4x4 - 1000x: (1)"
+python buscas.py 4 4 1000
+echo "Busca A* com heurística 2 - 4x4 - 1000x: (2)"
+python buscas.py 4 4 1000
+echo "Busca A* com heurística 2 - 4x4 - 1000x: (3)"
+python buscas.py 4 4 1000
+echo "Busca A* com heurística 2 - 4x4 - 10000x: (1)"
+python buscas.py 4 4 10000
+echo "Busca A* com heurística 2 - 4x4 - 10000x: (2)"
+python buscas.py 4 4 10000
+echo "Busca A* com heurística 2 - 4x4 - 10000x: (3)"
+python buscas.py 4 4 10000
